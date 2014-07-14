bb1f78b865cbf4e9014bbc7aa20185f62988a616
RapbatLe <slavic.rabota@gmail.com>
2014-07-14 18:10:39 +0300

    Test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+	Collapsed Left Navigation
+ */
+
+$(".collapse-btn").click(function() {
+		var mybtn = $(this);
+		var par = mybtn.parents("li").children(".collapse");
+		par.slideToggle(200);
+		$(mybtn).toggleClass("close");
+});
