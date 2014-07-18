f60b4c1955f3a846e85e41a7bd6f1ee22da6d188
RapbatLe <slavic.rabota@gmail.com>
2014-07-18 10:22:27 +0300

    Add icons for sidebar menu and header, and fix some small bugs

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(".collapse-btn").click(function() {
 		var mybtn = $(this);
 		var par = mybtn.parents("li").children(".collapse");
 		par.slideToggle(200);
-		$(mybtn).toggleClass("closesdas");
+		$(mybtn).toggleClass("close-menu");
 });
 
 $(function () { $("[data-toggle='tooltip']").tooltip(); });
